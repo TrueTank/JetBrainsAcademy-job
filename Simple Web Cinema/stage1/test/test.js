06eecb682ce546826f9d78ed0135fda13c62a45d
TrueTank <mosta001@mail.ru>
2022-08-11 13:55:22 +0500

    Обновила package-json, исправила тесты к первой стадии

--- Simple Web Cinema/stage1/test/test.js
+++ Simple Web Cinema/stage1/test/test.js
@@ -7,40 +7,43 @@ class Test extends StageTest {
     page = this.getPage(pagePath)
 
     tests = [
+        // Test 1 - check nav, title and login elements
         this.page.execute(() => {
-            let navElements = document.getElementsByClassName('nav');  
-            let orgElements = document.getElementsByClassName('Org__Title'); 
-            let logElements = document.getElementsByClassName('Log'); 
-            
-            if (navElements.length === 0 && orgElements === 0 && logElements === 0) {
+            let navElements = document.getElementsByClassName('nav');
+            let orgElements = document.getElementsByClassName('Org__Title');
+            let logElements = document.getElementsByClassName('Log');
+
+            if (navElements.length === 0 && orgElements.length === 0 && logElements.length === 0) {
                 return wrong(`Cannot find element with class 'nav', 'Org__Title' and 'Log`);
-            } else if (navElements > 1) {
+            } else if (navElements.length > 1) {
                 return wrong(`Found ${navElements.length} elements with class 'nav'` +
                     `, the page should contain just a single such element.`);
-            }       
+            }
 
             return correct()
         }),
+        // Test 2 - check main-container, box and video-container elements
         this.page.execute(() => {
-            let containerElements = document.getElementsByClassName('main__container');  
-            let boxElements = document.getElementsByClassName('box'); 
-            let videocontainerlements = document.getElementsByClassName('video-container'); 
-            
-            if (containerElements === 0 && boxElements === 0 && videocontainerlements === 0) {
+            let containerElements = document.getElementsByClassName('main__container');
+            let boxElements = document.getElementsByClassName('box');
+            let videoContainerElements = document.getElementsByClassName('video-container');
+
+            if (containerElements.length === 0 && boxElements.length === 0 && videoContainerElements.length === 0) {
                 return wrong(`Cannot find element with class 'main__container', 'box' and 'video-container`);
             } else if (containerElements > 1) {
                 return wrong(`Found ${containerElements.length} elements with class 'nav'` +
                     `, the page should contain just a single such element.`);
-            }       
+            }
 
             return correct()
         }),
+        // Test 3 - check body background color
         this.page.execute(() => {
-            const bodyStyles = await body.getStyles()
+            const bodyBackground = window.getComputedStyle(document.body).backgroundColor;
 
-            if (bodyStyles.backgroundColor !== '#f2ebff') {
-                return wrong('wrong backgroundColor')
-            } else 
+            if (bodyBackground !== 'rgb(242, 235, 255)') {
+                return wrong(`Wrong body background color, correct color is rgb(242, 235, 255), now - ${bodyBackground}`)
+            } else
                 return correct()
         })
     ]
